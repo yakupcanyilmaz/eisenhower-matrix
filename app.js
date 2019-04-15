1fcd389235fdccdfd8fea96a89e8b8301100a2aa
DESKTOP-UO8E75E\Can <ycanyilmaz@hotmail.com>
2019-04-15 19:36:14 +0300

    code and UI updated

--- app.js
+++ app.js
@@ -1,46 +1,27 @@
-class DoFirst {
-  constructor(id, description, hour) {
-    this.id = id;
-    this.description = description;
-    this.hour = hour;
-  }
-}
-class Schedule {
-  constructor(id, description, hour) {
-    this.id = id;
-    this.description = description;
-    this.hour = hour;
-  }
-}
-class Delegate {
-  constructor(id, description, hour) {
-    this.id = id;
-    this.description = description;
-    this.hour = hour;
-  }
-}
-class Eliminate {
-  constructor(id, description, hour) {
-    this.id = id;
-    this.description = description;
-    this.hour = hour;
-  }
-}
-
 const data = {
-  allTasks: {
+  tasks: {
     doFirst: [],
     schedule: [],
     delegate: [],
     eliminate: []
   },
-  totals: {
+  totalHours: {
     doFirst: 0,
     schedule: 0,
     delegate: 0,
     eliminate: 0
   }
 };
+
+// Task Class
+class Task {
+  constructor(type, id, desc, hour) {
+    this.type = type;
+    this.id = id;
+    this.desc = desc;
+    this.hour = hour;
+  }
+}
 
 // UI CLASS
 class UI {
@@ -49,203 +30,112 @@ class UI {
     this.importanceInput = document.querySelector(".add-importance-type");
     this.descriptionInput = document.querySelector(".add-description");
     this.hourInput = document.querySelector(".add-hour");
-    this.doFirstList = document.querySelector(".do-first-list");
-    this.scheduleList = document.querySelector(".schedule-list");
-    this.delegateList = document.querySelector(".delegate-list");
-    this.eliminateList = document.querySelector(".eliminate-list");
-    this.doFirstTotalHour = document.querySelector(".do-first-total-hour");
-    this.scheduleTotalHour = document.querySelector(".schedule-total-hour");
-    this.delegateTotalHour = document.querySelector(".delegate-total-hour");
-    this.eliminateTotalHour = document.querySelector(".eliminate-total-hour");
+    this.doFirstTotalHours = document.querySelector(".doFirst-total-hours");
+    this.scheduleTotalHours = document.querySelector(".schedule-total-hours");
+    this.delegateTotalHours = document.querySelector(".delegate-total-hours");
+    this.eliminateTotalHours = document.querySelector(".eliminate-total-hours");
   }
 
-  testing() {
-    console.log(data);
-  }
+  getTask() {
+    const task = new Task();
 
-  getInput() {
     const urgency = this.urgencyInput.value;
     const importance = this.importanceInput.value;
-    let type;
+
     if (urgency === "urgent" && importance === "important") {
-      type = "doFirst";
+      task.type = "doFirst";
     } else if (urgency === "urgent" && importance === "not-important") {
-      type = "schedule";
+      task.type = "schedule";
     } else if (urgency === "not-urgent" && importance === "important") {
-      type = "delegate";
+      task.type = "delegate";
     } else if (urgency === "not-urgent" && importance === "not-important") {
-      type = "eliminate";
+      task.type = "eliminate";
     }
-    return {
-      type: type,
-      description: this.descriptionInput.value,
-      hour: parseFloat(this.hourInput.value)
-    };
-  }
 
-  createTask(type, description, hour) {
-    let newTask, ID;
+    task.desc = this.descriptionInput.value;
+    task.hour = parseFloat(this.hourInput.value);
 
-    if (data.allTasks[type].length > 0) {
-      ID = data.allTasks[type][data.allTasks[type].length - 1].id + 1;
+    if (data.tasks[task.type].length > 0) {
+      task.id = data.tasks[task.type][data.tasks[task.type].length - 1].id + 1;
     } else {
-      ID = 0;
+      task.id = 0;
     }
 
-    if (type === "doFirst") {
-      newTask = new DoFirst(ID, description, hour);
-    } else if (type === "schedule") {
-      newTask = new Schedule(ID, description, hour);
-    } else if (type === "delegate") {
-      newTask = new Delegate(ID, description, hour);
-    } else if (type === "eliminate") {
-      newTask = new Eliminate(ID, description, hour);
-    }
-
-    // Push it into data structure
-    data.allTasks[type].push(newTask);
-
-    // Return the new element
-    return newTask;
-  }
-
-  addTaskToMatrix(task, type) {
-    let html, element;
-
-    if (type === "doFirst") {
-      element = this.doFirstList;
-      html = `
-      <div class="item clearfix" id="doFirst-${task.id}">
-        <div class="item-description">${task.description}</div>
-        <div class="hour">
-        <div class="item-hour">${task.hour}</div>
-          <div class="item-delete">
-            <button class="item-delete-btn">
-            <i class="ion-ios-close-outline"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-      `;
-    } else if (type === "schedule") {
-      element = this.scheduleList;
-      html = `
-      <div class="item clearfix" id="schedule-${task.id}">
-        <div class="item-description">${task.description}</div>
-        <div class="hour">
-        <div class="item-hour">${task.hour}</div>
-          <div class="item-delete">
-            <button class="item-delete-btn">
-            <i class="ion-ios-close-outline"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-      `;
-    } else if (type === "delegate") {
-      element = this.delegateList;
-      html = `
-      <div class="item clearfix" id="delegate-${task.id}">
-        <div class="item-description">${task.description}</div>
-        <div class="hour">
-        <div class="item-hour">${task.hour}</div>
-          <div class="item-delete">
-            <button class="item-delete-btn">
-            <i class="ion-ios-close-outline"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-      `;
-    } else if (type === "eliminate") {
-      element = this.eliminateList;
-      html = `
-      <div class="item clearfix" id="eliminate-${task.id}">
-        <div class="item-description">${task.description}</div>
-        <div class="hour">
-        <div class="item-hour">${task.hour}</div>
-          <div class="item-delete">
-            <button class="item-delete-btn">
-            <i class="ion-ios-close-outline"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-    `;
-    }
-    element.insertAdjacentHTML("beforeend", html);
-  }
-
-  calculateTotal(type) {
-    let sum = 0;
-    data.allTasks[type].forEach(function(cur) {
-      sum += cur.hour;
-    });
-    data.totals[type] = sum;
-  }
-
-  getTotals() {
-    return {
-      doFirstTotal: data.totals.doFirst,
-      scheduleTotal: data.totals.schedule,
-      delegateTotal: data.totals.delegate,
-      eliminateTotal: data.totals.eliminate
-    };
-  }
-
-  displayTotals() {
-    let totals;
-    totals = this.getTotals();
-    this.doFirstTotalHour.textContent = totals.doFirstTotal;
-    this.scheduleTotalHour.textContent = totals.scheduleTotal;
-    this.delegateTotalHour.textContent = totals.delegateTotal;
-    this.eliminateTotalHour.textContent = totals.eliminateTotal;
-  }
-
-  updateTotals() {
-    this.calculateTotal("doFirst");
-    this.calculateTotal("schedule");
-    this.calculateTotal("delegate");
-    this.calculateTotal("eliminate");
-    this.displayTotals();
+    return task;
   }
 
   addTask() {
-    let input, task;
-    input = this.getInput();
-    console.log(input);
-    if (input.description !== "" && !isNaN(input.hour) && input.hour > 0) {
-      task = this.createTask(input.type, input.description, input.hour);
-      this.addTaskToMatrix(task, input.type);
-      this.clearFields();
-    }
+    const newTask = this.getTask();
+
+    // Add task to data
+    data.tasks[newTask.type].push(newTask);
+
+    // Add task to matrix
+    let html, element;
+
+    element = document.querySelector(`.${newTask.type}-list`);
+    html = `
+      <div class="item clearfix" id="${newTask.type}-${newTask.id}">
+        <div class="item-description">${newTask.desc}</div>
+        <div class="hour">
+        <div class="item-hour">${newTask.hour}</div>
+          <div class="item-delete">
+            <button class="item-delete-btn">
+            <i class="ion-ios-close-outline"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      `;
+
+    element.insertAdjacentHTML("beforeend", html);
+    this.clearFields();
+  }
+
+  getTotalHours() {
+    let sum = 0;
+    const type = this.getTask().type;
+    data.tasks[type].forEach(function(cur) {
+      sum += cur.hour;
+    });
+    data.totalHours[type] = sum;
+    return data.totalHours;
+  }
+
+  updateTotalHours() {
+    const totalHours = this.getTotalHours();
+    this.doFirstTotalHours.textContent = totalHours.doFirst;
+    this.scheduleTotalHours.textContent = totalHours.schedule;
+    this.delegateTotalHours.textContent = totalHours.delegate;
+    this.eliminateTotalHours.textContent = totalHours.eliminate;
   }
 
   deleteTask(target) {
     let taskID, splitID, type, ID;
     taskID = target.parentNode.parentNode.parentNode.parentNode.id;
+
     if (taskID) {
       splitID = taskID.split("-");
+
       type = splitID[0];
       ID = parseInt(splitID[1]);
-      console.log(type, ID);
+
       this.deleteFromData(type, ID);
       this.deleteFromMatrix(taskID);
     }
+    this.clearFields();
   }
 
   deleteFromData(type, id) {
     let ids, index;
-    ids = data.allTasks[type].map(function(cur) {
+    ids = data.tasks[type].map(function(cur) {
       return cur.id;
     });
 
     index = ids.indexOf(id);
-    console.log(ids);
-    console.log(id);
-    console.log(index);
+
     if (index !== -1) {
-      data.allTasks[type].splice(index, 1);
+      data.tasks[type].splice(index, 1);
     }
   }
 
@@ -254,28 +144,55 @@ class UI {
     el.parentNode.removeChild(el);
   }
 
+  showAlert(message, className) {
+    const div = document.createElement("div");
+    div.className = `alert ${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector("#matrix");
+    const form = document.querySelector(".matrix-container");
+    container.insertBefore(div, form);
+
+    // Timeout after 3 sec
+    setTimeout(function() {
+      document.querySelector(".alert").remove();
+    }, 3000);
+  }
+
   clearFields() {
     this.descriptionInput.value = "";
     this.hourInput.value = "";
   }
 }
 
-document.querySelector(".add-btn").addEventListener("click", function(e) {
+function eventListeners() {
+  const addButton = document.querySelector(".add-btn");
+  const matrix = document.querySelector(".matrix-container");
+
   const ui = new UI();
 
-  ui.addTask();
-  ui.updateTotals();
-  ui.testing();
+  addButton.addEventListener("click", function(e) {
+    // Validate
+    if (
+      ui.descriptionInput.value === "" ||
+      Math.sign(parseFloat(ui.hourInput.value)) === -1
+    ) {
+      // Error alert
+      ui.showAlert("Please enter valid value", "error");
+    } else {
+      ui.addTask();
+      ui.updateTotalHours();
+    }
+    e.preventDefault();
+  });
 
-  e.preventDefault();
-});
-document
-  .querySelector(".matrix-container")
-  .addEventListener("click", function(e) {
-    const ui = new UI();
-
+  matrix.addEventListener("click", function(e) {
     ui.deleteTask(e.target);
-    ui.updateTotals();
+    ui.updateTotalHours();
 
     e.preventDefault();
   });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  eventListeners();
+});
